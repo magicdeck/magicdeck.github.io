49023d0e31c67a57a5c541a3105b746c0ee4e705
adiog <adiog@brainfuck.pl>
2018-02-11 17:39:48 +0100

    more saple decks

--- js/utils/ContextManager.js
+++ js/utils/ContextManager.js
@@ -211,8 +211,15 @@ class ContextManager {
             else {
                 this.selectedKey = this.defaultKey;
                 this.context = copy(this.defaultContext);
-                this.contextCollection = {};
-                this.contextCollection[this.selectedKey] = copy(this.context);
+                this.contextCollection = {
+  "XLN BR Pirates": {"deck": "1 Deadeye Tracker\n3 Rigging Runner\n2 Kitesail Freebooter\n1 Fathom Fleet Captain\n3 Dire Fleet Captain\n3 Dire Fleet Poisoner\n1 Dire Fleet Daredevil\n1 Ruin Raider\n2 Dire Fleet Neckbreaker\n \n1 March of the Drowned\n2 Fiery Cannonade\n2 Fell Flagship\n1 Vanquisher's Banner\n \n3 Unclaimed Territory\n3 Dragonskull Summit\n6 Mountain\n5 Swamp", "sets": ""},
+"AKH BG -1/-1 Tokens": {"deck": "3 Hapatra, Vizier of Poisons\n3 Channeler Initiate\n3 Obelisk Spider\n3 Ammit Eternal\n2 Decimator Beetle\n \n2 Hapatra's Mark\n2 Nest of Scarabs\n2 Splendid Agony\n2 Torment of Venom\n1 Overcome\n \n2 Evolving Wilds\n2 Foul Orchard\n2 Ifnir Deadlands\n5 Swamp\n6 Forest", "sets": ""},
+"AKH GW Cats": {"deck": "3 Sacred Cat\n3 Adorned Pouncer\n3 Feral Prowler\n3 Pride Sovereign\n1 Prowling Serpopard\n2 Regal Caracal\n \n2 Prepare\n3 Cast Out\n2 Anointed Procession\n1 Overcome\n \n2 Endless Sands\n9 Plains\n6 Forest", "sets": ""},
+"AKH WB Zombies": {"deck": "3 Dread Wanderer\n3 Wayward Servant\n3 Lord of the Accursed\n1 Ammit Eternal\n2 Unraveling Mummy\n1 Merciless Eternal\n1 Dreamstealer\n1 Gravedigger\n1 Accursed Horde\n \n2 Grind\n1 Torment of Hailfire\n1 Torment of Scarabs\n2 Liliana's Mastery\n1 Stir the Sands\n \n2 Cradle of the Accursed\n2 Evolving wilds\n2 Forsaken Sanctuary\n2 Plains\n9 Swamp", "sets": ""},
+"KLD UG Energy": {"deck": "2 Greenbelt Rampager\n3 Servant of the Conduit\n1 Shielded Aether Thief\n3 Rogue Refiner\n2 Empyreal Voyager\n3 Bristling Hydra\n1 Rashmi, Eternities Crafter\n1 Aetherwind Basker\n \n3 Attune with Aether\n2 Blossoming Defense\n1 Heroic Intervention\n1 Glimmer of Genius\n \n3 Aether Hub\n2 Island\n12 Forest", "sets": ""},
+"KLD WR Vehicles": {"deck": "3 Speedway Fanatic\n3 Veteran Motorist\n1 Sram, Senior Edificer\n1 Renegade Wheelsmith\n1 Solemn Recruit\n1 Aerial Responder\n2 Depala, Pilot Exemplar\n\n1 Built to Last\n2 Hungry Flames\n1 Renegade Freighter\n3 Peacewalker Colossus\n1 Fleetwheel Cruiser\n1 Bomat Bazaar Barge\n2 Skysovereign, Consul Flagship\n\n3 Stone Quarry\n7 Plains\n7 mountain", "sets": ""}
+                };
+                this.contextCollection[this.selectedKey] = copy(this.contextCollection["AKH GW Cats"]);
             }
         }
 
